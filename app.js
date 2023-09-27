22901a8289a7aae306dfe6ab60becbe51db0261c
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-27 21:56:03 +0200

    Setup CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
