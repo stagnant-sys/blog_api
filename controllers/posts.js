03279182858ccdaebc9ee655782a6589d5829efb
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-27 22:26:26 +0200

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,13 +6,13 @@ const BlogPost = require('../models/post');
 
 // GET all posts
 exports.posts_index_get = asyncHandler(async (req, res, next) => {
-  const posts = await BlogPost.find({ visibility: 'public' }).populate('author').exec();
+  const posts = await BlogPost.find({ visibility: 'public' }).exec();
   res.json(posts);
 });
 
 
 // GET post detail (text, comments)
 exports.post_detail_get = asyncHandler(async (req, res, next) => {
-  const post = await BlogPost.findById(req.params.id).populate('author').exec();
+  const post = await BlogPost.findById(req.params.id).exec();
   res.json(post);
 });
