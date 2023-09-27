9fb508980564a4df4cea19a26e6b3eff65b422bb
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-27 22:17:24 +0200

    Populate author field on request

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,13 +6,13 @@ const BlogPost = require('../models/post');
 
 // GET all posts
 exports.posts_index_get = asyncHandler(async (req, res, next) => {
-  const posts = await BlogPost.find({ visibility: 'public' });
+  const posts = await BlogPost.find({ visibility: 'public' }).populate('author');
   res.json(posts);
 });
 
 
 // GET post detail (text, comments)
 exports.post_detail_get = asyncHandler(async (req, res, next) => {
-  const post = await BlogPost.findById(req.params.id);
+  const post = await BlogPost.findById(req.params.id).populate('author');
   res.json(post);
 });
