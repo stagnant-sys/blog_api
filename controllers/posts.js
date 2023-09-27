41e94fde28f6c084ca2e85106226ad222688fb53
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-27 22:42:44 +0200

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const asyncHandler = require("express-async-handler");
 
 const BlogPost = require('../models/post');
+const User = require('../models/user');
 
 // GET all posts
 exports.posts_index_get = asyncHandler(async (req, res, next) => {
-  const posts = await BlogPost.find({})
+  const posts = await BlogPost.find({ visibility: 'public' })
     .sort({ timestamp: -1 })
     .populate('author')
     .exec();
