db9d7bb6cd0c572bbba11636ef43540d41cb1297
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-29 21:29:24 +0200

    Add route and controller for getting all posts (including private)

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,14 +6,23 @@ const BlogPost = require('../models/post');
 const User = require('../models/user');
 const Comment = require('../models/comment');
 
-// GET all posts
-exports.posts_index_get = asyncHandler(async (req, res, next) => {
+// GET all public posts
+exports.posts_index_public_get = asyncHandler(async (req, res, next) => {
   const posts = await BlogPost.find({ visibility: 'public' })
     .sort({ timestamp: -1 })
     .populate('author', 'username')
     .exec();
   res.json(posts);
 });
+
+// GET all posts (private and public)
+exports.post_index_get = asyncHandler(async (req, res, next) => {
+  const posts = await BlogPost.find({})
+    .sort({ timestamp: -1 })
+    .populate('author')
+    .exec();
+  res.json(posts);
+})
 
 
 // GET post detail (text, comments)
@@ -22,8 +31,5 @@ exports.post_detail_get = asyncHandler(async (req, res, next) => {
     await BlogPost.findById(req.params.id).populate('author', 'username').exec(),
     await Comment.find({ post : req.params.id }).populate('author', 'username').exec(),
   ])
-  /*const post = await BlogPost.findById(req.params.id)
-    .populate('author')
-    .exec();*/
   res.json({post, comments});
 });
--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const posts_controller = require('../controllers/posts');
 
 
-// GET all posts
-router.get('/', posts_controller.posts_index_get);
+// GET all public posts
+router.get('/', posts_controller.posts_index_public_get);
+
+// GET all posts (private and public)
+router.get('/all', posts_controller.posts_index_get);
 
 // GET post detail
 router.get('/:id', posts_controller.post_detail_get);
