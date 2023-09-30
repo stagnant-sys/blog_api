7e297f6471931657bf1a3dfa8439ba73a0d2025a
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-09-30 07:21:14 +0200

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const posts_controller = require('../controllers/posts');
 
 
 // GET all public posts
-router.get('/', posts_controller.posts_index_public_get);
+router.get('/public', posts_controller.posts_index_public_get);
 
 // GET all posts (private and public)
 router.get('/all', posts_controller.posts_index_get);
