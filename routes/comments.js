128fa8ac78a14e5b0625cade371857fc481c37d0
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-02 16:12:22 +0200

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const comments_controller = require('../controllersc/comments');
 
-router.delete('/:id/delete', comments_controller.delete_comment_delete)
+router.delete('/:id/delete', comments_controller.delete_comment_delete);
+
+module.exports = router;
