44e59b332dd66a978a87ac6f9912188d1e654355
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-03 21:34:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const session = require("express-session");
 const passport = require("passport");
+const bcrypt = require('bcryptjs');
 const LocalStrategy = require("passport-local").Strategy;
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
