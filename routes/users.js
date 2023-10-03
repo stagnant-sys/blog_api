3d646593a3ada85f231ccf2665667b6ddf9555db
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-03 22:26:41 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,8 @@ router.post('/signup', users_controller.user_signup_post);
 router.post('/login', 
   passport.authenticate('local', { failureRedirect: '/', failureMessage: true }),
   function(req, res) {
-    res.redirect('/~' + req.user.username);
+    const data = {user: req.user.username}
+    res.json(data);
   }
 );
 
