4b8b3fdc9f8160e39df16b6dc748e4a61ecaea08
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-04 20:42:49 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -76,23 +76,22 @@ exports.user_signup_post = [
 
 exports.user_login_post = asyncHandler(async (req, res, next) => {
   try {
-    passport.authenticate('local', {session: false}, (err, user, info) =>{
+    passport.authenticate ('local', {session: false}, (err, user, info) =>{
       if (err || !user){
         const error = new Error('User does not exist')
         return res.status(403).json({
           info
         })
       }
-    req.login(user, {session: false}, (err) => {
+    req.login (user, {session: false}, (err) => {
       if (err){
         next(err);
       }
-      // create token
       const body = {_id: user._id, username: user.username, admin: user.admin}
       return res.status(200).json({body});
     });
   }) (req, res, next);
-  } catch (err){
+  } catch (err) {
     res.status(403).json({
       err
     })
