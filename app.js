fe489c6aeef72da63ad1fe080d38bc13d8c73922
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-04 08:05:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ const User = require('./models/user');
 const app = express();
 
 // Passport setup
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" })
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -48,24 +67,6 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Passport
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      };
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: "Incorrect password" })
-      }
-      return done(null, user);
-    } catch(err) {
-      return done(err);
-    };
-  })
-);
 
 app.use(logger('dev'));
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
