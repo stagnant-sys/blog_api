33dbdfec46b226b9bc9d4779d8e2e87750a6410f
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-04 09:22:46 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -75,7 +75,7 @@ exports.user_signup_post = [
 ]*/
 
 exports.user_login_post = (req, res, next) => {
-  res.send('Hello');
+  res.send(req.body);
   /*passport.authenticate('local', { failureRedirect: '/login' }),
   function(req, res) {
     
