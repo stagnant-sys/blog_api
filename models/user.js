5e2a9cea0ac345e84b9c98d484ac1ebcf95577b5
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-05 07:53:14 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema ({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  role: { type: String }
 })
 
 module.exports = mongoose.model('User', UserSchema);
