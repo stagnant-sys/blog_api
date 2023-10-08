8a3aaa8e784981abc87d7ecd6576b6d44371431d
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-08 09:55:11 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -43,7 +43,7 @@ exports.user_signup_post = [
     });
 
     if (!errors.isEmpty()) {
-      res.sendStatus(500).json(errors.array());
+      res.json(errors.array()).sendStatus(500);
       return;
     } else {
       bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
