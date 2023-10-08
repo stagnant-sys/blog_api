708f54ca2d2a48bd96e69bae39c754d3cca95fdb
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-08 07:13:44 +0200

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -32,6 +32,9 @@ exports.post_detail_get = asyncHandler(async (req, res, next) => {
     await BlogPost.findById(req.params.id).populate('author', 'username').exec(),
     await Comment.find({ post : req.params.id }).populate('author', 'username').exec(),
   ])
+  if (!post) {
+    res.sendStatus(404);
+  }
   res.json({post, comments});
 });
 
