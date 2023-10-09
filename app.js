a26fa1d74871b544ca204b68ab0ed62969fcf4a5
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-09 14:19:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -80,9 +80,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: 'https://scientized.netlify.app/'
-}));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
